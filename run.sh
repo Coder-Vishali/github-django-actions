aeb26e71a9ef25cae8eb41dca8f9c56e31fc98d3
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-01-11 12:33:18 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh     
+sudo apt-get update
+sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
+sudo pip3 install virtualenv
+sudo apt install git-all
 sudo git pull origin main
 sudo pip install -r requirements.txt
 python manage.py makemigrations

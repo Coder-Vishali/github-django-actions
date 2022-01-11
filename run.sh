a981e5fa7d60e8ab2179a6d8501e602b3b1be028
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-01-11 12:57:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,2 @@
 #!/bin/sh     
-sudo apt-get update
-sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
-sudo apt install git-all
-sudo apt install python-pip
-sudo pip3 install virtualenv
-#sudo git pull origin main
-#sudo virtualenv myprojectenv
-#source myprojectenv/bin/activate
-sudo pip install -r requirements.txt
-sudo pip install django
-django-admin startproject myproject
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
-python manage.py runserver 0.0.0.0:8000
+python test.py

933c87635d6b7929b60cd32fd67ca5f92e85dcb2
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-01-12 11:32:13 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
 #!/bin/sh    
-
-#cd /home/ubuntu/github-django-actions
-#source myprojectenv/bin/activate
-#export DJANGO_SETTINGS_MODULE="config.settings.myproject"
-#python manage.py runserver 0.0.0.0:8000

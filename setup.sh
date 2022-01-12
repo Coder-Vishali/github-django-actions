38f379e8a16b79ef93d99cdaae65957b8c771566
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-01-12 11:26:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get -y upgrade
+#sudo apt-get update
+#sudo apt-get -y upgrade
 
 # Destination folder
 cd /home/ubuntu/github-django-actions
 # AWS CLI needed to get parameters
-sudo apt-get install -y awscli
+#sudo apt-get install -y awscli
 
-sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
-sudo apt install git-all
-sudo pip3 install virtualenv
-python3 -m ensurepip --default-pip --user
-sudo pip install django
+#sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
+#sudo apt install git-all
+#sudo pip3 install virtualenv
+#python3 -m ensurepip --default-pip --user
+#sudo pip install django
 
 #python3 -m venv myprojectenv
 #source myprojectenv/bin/activate
